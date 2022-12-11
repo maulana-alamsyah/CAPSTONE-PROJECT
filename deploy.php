db169c673c62952376ac0da11257b02f8c5f96fd
maulana-alamsyah <maulanalamsyah@gmail.com>
2022-12-11 10:24:32 +0700

    add deployer

---
+++ deploy.php
@@ -0,0 +1,22 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/maulana-alamsyah/CAPSTONE-PROJECT.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('47.250.41.118')
+    ->set('remote_user', 'root')
+    ->set('deploy_path', '/var/www/html/staging/CAPSTONE-PROJECT');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
